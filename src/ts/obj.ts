bce5867df11272da3a8a0cce53a2d61afba195d9
Abhishek Jain <com.dsynr@gmail.com>
2020-04-24 11:07:08 -0400

    +updateProps

--- src/ts/obj.ts
+++ src/ts/obj.ts
@@ -6,7 +6,15 @@ function get_rand_array_item(mixed_arr: any): any {
     return mixed_arr[Math.floor(Math.random() * mixed_arr.length)];
 }
 
-function get_rand_obj_item(obj: Object) {
+function get_rand_obj_item(obj: object) {
     let keys: Array<any> = Object.keys(obj);
     return obj[keys[keys.length * Math.random() << 0]];
+}
+
+function updateProps(obj: Object, options: any): void {
+    for (let option in options) {
+        if (options.hasOwnProperty(option)) {
+            obj[option] = option;
+        }
+    }
 }
