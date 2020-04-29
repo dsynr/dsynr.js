fba9924a25b43a9398898fc39fce43116715c15e
Abhishek Jain <com.dsynr@gmail.com>
2020-04-29 18:52:16 -0400

    updated addClass, removeClass

--- src/js/css.js
+++ src/js/css.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param val
+ * @param unit
+ */
 function getCssDimension(val, unit = 'px') {
     return val + unit;
 }
@@ -7,18 +12,41 @@ function randRadius() {
 function randWidth() {
     return getRandomArbitrary(2, 15);
 }
+/**
+ *
+ * @param e
+ */
 function getDimensions(e) {
     return { w: e.clientWidth, h: e.clientHeight };
 }
+/**
+ *
+ * @param e
+ */
 function hide(e) {
     e.style.display = 'none';
 }
+/**
+ *
+ * @param e
+ * @param classes
+ */
 function addClass(e, classes) {
-    e.className += ' ' + classes;
+    e.classList.add(...classes.split(' '));
 }
+/**
+ *
+ * @param e
+ * @param classes
+ */
 function removeClass(e, classes) {
-    e.classList.remove(classes);
+    e.classList.remove(...classes.split(' '));
 }
+/**
+ *
+ * @param e
+ * @param classes
+ */
 function hasClass(e, classes) {
     return e.classList.contains(classes);
 }
--- src/ts/css.ts
+++ src/ts/css.ts
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param val
+ * @param unit
+ */
 function getCssDimension(val: number, unit: string = 'px'): string {
     return val + unit;
 }
@@ -10,22 +15,45 @@ function randWidth(): number {
     return getRandomArbitrary(2, 15);
 }
 
+/**
+ *
+ * @param e
+ */
 function getDimensions(e: HTMLElement): any {
     return {w: e.clientWidth, h: e.clientHeight};
 }
 
+/**
+ *
+ * @param e
+ */
 function hide(e: HTMLElement): void {
     e.style.display = 'none';
 }
 
+/**
+ *
+ * @param e
+ * @param classes
+ */
 function addClass(e: HTMLElement, classes: string): void {
-    e.className += ' ' + classes;
+    e.classList.add(...classes.split(' '));
 }
 
+/**
+ *
+ * @param e
+ * @param classes
+ */
 function removeClass(e: HTMLElement, classes: string): void {
-    e.classList.remove(classes);
+    e.classList.remove(...classes.split(' '));
 }
 
+/**
+ *
+ * @param e
+ * @param classes
+ */
 function hasClass(e: HTMLElement, classes: string): boolean {
     return e.classList.contains(classes);
 }
