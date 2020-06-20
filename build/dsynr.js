eee3939f14ee80b71d5bc99c4babab28417da74d
Abhishek Jain <com.dsynr@gmail.com>
2020-06-20 18:28:58 -0400

    minor refinements; restructuring

--- build/dsynr.js
+++ build/dsynr.js
@@ -957,3 +957,33 @@ class Dsynr {
 }
 let d = new Dsynr();
 //# sourceMappingURL=Dsynr.js.map
+class DsynrWp {
+    constructor() {
+        this.conf = {
+            formURL: 'form/',
+        };
+    }
+    ready() {
+        d.lfn('DsynrUtilWp');
+        d.getPageScripts(d);
+    }
+    getForm(formName, parent = d.conf.defaultParent) {
+        d.lfn('getForm');
+        if (d.requestDataset[formName] != undefined) {
+            d.l(formName + ' Previously loaded / Reinstantiating from memory...');
+            d.addFetchedData(d.requestDataset[formName], parent);
+        }
+        else {
+            d.ajax(d.conf.domain + this.conf.formURL + formName + '?min', formName);
+        }
+    }
+    ajax(params = {}, parent = d.conf.defaultParent) {
+        d.lfn('ajax / ' + name);
+        let formData = d.mergeObjs({ action: 'dw_ajax' }, params);
+        d.l(formData);
+        // @ts-ignore
+        d.ajax(ajxRequest.ajaxurl, false, formData, true, parent, 'POST');
+    }
+}
+let dw = new DsynrWp();
+//# sourceMappingURL=DsynrWp.js.map
